8651eb5af7baae72cfc3dc87cc0321a600600697
Matheus Barni <matheusbarni@Mac-mini-de-Giovani.local>
2020-12-16 13:53:05 -0300

    first commit

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components"
+
+export default createGlobalStyle`
+  * {
+    margin: 9;
+    padding: 0;
+  }
+  body {
+    background: #121214;
+    color: #ffffff;
+  }
+`;
